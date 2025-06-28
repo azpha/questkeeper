ee666d7511564f909199b539f1c3e65325d10a9c
Alex Frantz <alex@alexav.gg>
2025-06-28 02:31:25 -0400

    add empty state to home page

--- apps/frontend/src/pages/App.tsx
+++ apps/frontend/src/pages/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import GameCard from "@/components/Game/GameCard";
 import Layout from "@/components/Layout";
@@ -17,11 +18,32 @@ function App() {
   return (
     <Layout>
       <div className="container flex justify-center mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 my-2">
-          {games?.map((v, k) => {
-            return <GameCard key={k} game={v} />;
-          })}
-        </div>
+        {games && games.length < 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 my-2">
+            {games?.map((v, k) => {
+              return <GameCard key={k} game={v} />;
+            })}
+          </div>
+        ) : (
+          <div className="flex flex-col items-center">
+            <div className="my-6">
+              <h1 className="text-2xl font-bold">
+                Unlimited games, but no games?
+              </h1>
+              <p>There's nothing here. Why not start your collection?</p>
+              <div className="text-center my-2">
+                <Link to="/search">
+                  <button
+                    type="button"
+                    className="hover:cursor-pointer bg-white text-black rounded-lg p-2 font-semibold"
+                  >
+                    Take me awaaaayyyy
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </Layout>
   );
